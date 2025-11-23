a4bca22d3bf2649e872c9892f7c845fd0e342e5d
Asus <Asus@DESKTOP-GCS9LO9>
2025-11-23 20:08:23 +0200

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LupuCatalinaLab7")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LupuCatalinaLab7.Pages")]
