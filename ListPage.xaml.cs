a53c9147951b3cb9c7489a567a555c7feffef879
Cata716 <lupucatalina400@gmail.com>
2026-02-05 21:36:31 +0200

    final laborator 10

---
+++ ListPage.xaml.cs
@@ -0,0 +1,83 @@
+//using AuthenticationServices;
+using LupuCatalinaLab7.Models;
+namespace LupuCatalinaLab7;
+
+public partial class ListPage : ContentPage
+{
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        Shop selectedShop = (ShopPicker.SelectedItem as Shop);
+        slist.ShopID = selectedShop.ID;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var items = await App.Database.GetShopsAsync();
+        ShopPicker.ItemsSource = (System.Collections.IList)items;
+
+        ShopPicker.ItemDisplayBinding = new Binding("ShopDetails");
+
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+
+    }
+
+    async void OnDeleteItemButtonClicked(object sender, EventArgs e)
+    {
+        if(listView.SelectedItem == null)
+        {
+            await DisplayAlert("No Selection", "Please select a product first", "OK");
+            return;
+        }
+
+        var selectedProduct = listView.SelectedItem as Product;
+        var shopList = (ShopList)BindingContext;
+
+        bool answer = await DisplayAlert("Confirm Delete",
+                                         $"Delete '{selectedProduct.Description}' from list?",
+                                         "Yes", "No");
+
+        if (answer)
+        {
+            var listProduct = await App.Database.GetListProductAsync(shopList.ID, selectedProduct.ID);
+
+            if(listProduct != null)
+            {
+                await App.Database.DeleteListProductAsync(listProduct);
+
+                listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+
+                await DisplayAlert("Success", "Product deleted from list", "OK");
+            }
+        }
+
+    }
+}
